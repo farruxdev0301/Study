955fcf3b5b714d816cf353db3d28562a27a0019d
farruxdev0301 <cdshadow85@gmail.com>
2026-02-08 13:03:27 +0500

    study

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("HELLO, WORLD!")
+	fmt.Println("HELLO, WORLD!")
+	fmt.Println("HELLO, WORLD!")
+
+	fmt.Print("HELLO, WORLD!")
+	fmt.Print("HELLO, WORLD!")
+	fmt.Print("HELLO, WORLD!")
+} 
